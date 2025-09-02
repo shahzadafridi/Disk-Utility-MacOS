fb1f7f4289d401524374438e4d1ab4260975a3cf
shahzadahmad10 <shahzad.ahmad@venturedive.com>
2025-09-02 13:16:41 +0500

    Initial Commit

---
+++ DiskUtility/DiskUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskUtilityApp.swift
+//  DiskUtility
+//
+//  Created by shahzadahmad on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiskUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiskUtilityTests/DiskUtilityTests.swift
@@ -0,0 +1,16 @@
+//
+//  DiskUtilityTests.swift
+//  DiskUtilityTests
+//
+//  Created by shahzadahmad on 02/09/2025.
+//
+
+import Testing
+
+struct DiskUtilityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
